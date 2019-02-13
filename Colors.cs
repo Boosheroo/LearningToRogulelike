882b91a0b0ac59ac3f9ce333db6c76a71ea440d3
Boosheroo <35901960+Boosheroo@users.noreply.github.com>
2019-02-13 11:39:09 -0500

    Update Colors.cs

--- Colors.cs
+++ Colors.cs
@@ -15,5 +15,7 @@ namespace LearningtoRoguelike.Core
         public static RLColor WallFov = Swatch.SecondaryLighter;
 
         public static RLColor TextHeading = Swatch.DbLight;
+        
+        public static RLColor Player = Swatch.DbLight;
     }
 }
